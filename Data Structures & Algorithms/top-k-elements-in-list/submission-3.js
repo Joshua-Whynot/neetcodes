c24b368a5f9f175c829beed9886f3dbb88e42c64
Joshua Whynot <107290890+Joshua-Whynot@users.noreply.github.com>
2026-04-19 13:53:11 -0400

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+        for (const num of nums){
+            if (map.has(num)){
+                map.set(num, map.get(num) + 1);
+            } else {
+                map.set(num, 1);
+            }
+        }
+
+        const out = [];
+        let i = 0;
+        const sortedMap = new Map([...map].sort((a, b) => b[1] - a[1]));
+        //console.log(map);
+        //console.log(sortedMap);
+        for (const [key, value] of sortedMap) {
+            if (i<k){
+                out.push(key);
+                i++;
+            }
+        }
+        if (out.length > 0) {
+            return out
+        } else {
+            return nums
+        }
+    }
+}
